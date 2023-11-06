fbe95d2d2269a2226bd320494a13ae8504d81adb
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2023-11-06 14:16:33 -0800

    Starting 4.2

---
+++ Prototype 4 - Gameplay Mechanics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerRb;
+    private GameObject _focalPoint;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        _playerRb.AddForce(_focalPoint.transform.forward * speed * forwardInput);
+    }
+}
