529b12d199ddd045ad60c8e9f1cc404fec0d9e88
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2023-11-08 13:34:36 -0800

    Working on 4.3

--- Prototype 4 - Gameplay Mechanics/Assets/Scripts/PlayerController.cs
+++ Prototype 4 - Gameplay Mechanics/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _playerRb;
     private GameObject _focalPoint;
+    private float _powerupStrength = 15.0f;
     public float speed = 5.0f;
+    public bool hasPowerUp;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +22,33 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         _playerRb.AddForce(_focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Power-Up"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + " with power-up set to " + hasPowerUp);
+            enemyRigidbody.AddForce(awayFromPlayer * _powerupStrength, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
     }
 }
--- Prototype 4 - Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
+++ Prototype 4 - Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    private float spawnRange = 9;
+    private float _spawnRange = 9;
 
     // Start is called before the first frame update
     void Start()
-    {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+    { 
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-_spawnRange, _spawnRange);
+        float spawnPosZ = Random.Range(-_spawnRange, _spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
     }
 }
